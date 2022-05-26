69e4d6655e2e0969c45127dfd5614510cf5b86ea
Albert Byrone <albertbyrone1677@gmail.com>
2022-05-26 15:18:25 +0300

    commit tree e69471d116bb8c3b909f89d1273aef59da5c62ca

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def line(deli)
+    if deli.empty?
+        puts "The line is currently empty."
+    else
+        _current_line_= "The line is currently:"
+        deli.each.with_index(1) do | person, index|
+            _current_line_ << " #{index}. #{person}"
+        end
+        puts _current_line_
+    end
+end
+
+def take_a_number(deli,person)
+    deli << person
+    puts "Welcome, #{person}. You are number #{deli.length} in line."
+end
+def now_serving(deli)
+    if deli.empty?
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{deli.first}."
+        deli.shift
+    end
+end
